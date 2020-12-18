2f16a71016b1d1cdf44535727ab0b6dbcae7a053
root <root@retr0.home>
2020-12-18 21:00:01 +0100

    wersja pierwsza i ostatnia

---
+++ inter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   if(sizeof(int64_t))
+    {
+        cout << "komp 64 bitowy" << endl;
+    }
+    else
+    {
+        cout << "komp 32 bitowy" << endl;
+    }
+}
